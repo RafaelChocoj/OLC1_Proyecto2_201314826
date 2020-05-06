585d1d912e0564b43d0827a8af26a2aaa9c7a5ec
Rafael Chocoj <rafaelcliveguitar@gmail.com>
2020-05-06 03:01:35 -0600

    clint

---
+++ Client/conn/conexion.js
@@ -0,0 +1,16 @@
+function Send(){
+
+    //var texto = document.getElementById("operacion").value;
+    var tprin=document.getElementById('txtSalida').value;
+    console.log(tprin);
+
+    /*var url='http://localhost:8080/Calcular/';
+
+    $.post(url,{text:texto},function(data,status){
+        if(status.toString()=="success"){
+            alert("El resultado es: "+data.toString());
+        }else{
+            alert("Error estado de conexion:"+status);
+        }
+    });*/
+}
